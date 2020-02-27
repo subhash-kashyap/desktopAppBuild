75e9507bf94e15f71bbe1d1c6855d1612f7bea48
sh52 <sk.kashyap1997@gmail.com>
2020-02-27 22:43:09 +0530

    First Push

---
+++ desktopAppBuilder.sh
@@ -0,0 +1,25 @@
+echo "Builds Desktop Apps"
+
+##Input path absolute including name.sh
+echo "Provide Executable path" 
+
+##Input Desktop App name
+echo "Provide Execuable name"
+
+##Input Icon Path
+echo "Provide Icon Path"
+
+echo "What category is the app"
+
+#############
+#[Desktop Entry]
+#Version=1.0
+#Type=Application
+#Name=Android Studio
+#Exec="/home/username/Programs/AndroidStudio/bin/studio.sh" %f
+#Icon=/home/username/Programs/AndroidStudio/bin/idea.png
+#Categories=Development;IDE;
+#Terminal=false
+#############
+
+echo "cd absolute_path && chmod a+x somename.desktop"
